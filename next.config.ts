95eb2f51fc0f88c24bb9e556c24bfccb0f7d1ca1
dorin.turcanu <dorin.turcanu@fluence.tax>
2026-02-09 17:23:39 +0200

    bun nextjs update & upgrade

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type {NextConfig} from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
 };
 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default nextConfig;
